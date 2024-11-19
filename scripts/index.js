d57b6fda9a9603b615f297a67855d07b5bb1bcbe
kirill <khodarevskii@yandex.ru>
2024-11-19 21:15:29 +0300

    Загрузка стандартных карточик на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,24 @@
-// @todo: Темплейт карточки
+
 const elementPlace = document.querySelector('.places__list');
 const element = document.querySelector('#card-template').content;
-// @todo: DOM узлы
 
 initialCards.forEach(function (elements) {
     return renderCard(elements.link, elements.name);
   });
 
-// @todo: Функция создания карточки
-function createElement(imageValue, textValue) {
+function createElement(imageValue, textValue, deleteElement) {
     const userElemnt = document.querySelector('#card-template').content;
-    const element = userElemnt.querySelector('.places__item').cloneNode(true);
-    element.querySelector('.card__image').src = imageValue
-    element.querySelector('.card__title').textContent = textValue
-    const deleteButton = element.querySelector('.card__delete-button');
-    const image = element.querySelector('.card__image').src;
-    const text = element.querySelector('.card__title').textContent;
-    const likes = element.querySelector('.card__like-button');
-    deleteButton.addEventListener('click', deleteElement.bind('button', deleteButton));
-    return element;
+    const userElemntPlace = userElemnt.querySelector('.places__item').cloneNode(true);
+    userElemntPlace.querySelector('.card__image').src = imageValue
+    userElemntPlace.querySelector('.card__image').alt = textValue
+    userElemntPlace.querySelector('.card__title').textContent = textValue
+    const deleteButton = userElemntPlace.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteElement);
+    return userElemntPlace;
   }
-// @todo: Функция удаления карточки
-function deleteElement(target) {
-    target.parentElement.remove()
-  }
-
-// @todo: Вывести карточки на страницу
-
 
   function renderCard(img, text) {
-    return elementPlace.prepend(createElement(img, text));
+    return elementPlace.prepend(createElement(img, text, function (evt) {
+      evt.target.closest('.card').remove() 
+      }));
   }
