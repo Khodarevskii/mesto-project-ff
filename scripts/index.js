ed3b33f86571a9a175589a2f4a530b6549861f6a
kirill <khodarevskii@yandex.ru>
2024-11-21 00:51:27 +0300

    Загрузка стандартных карточик на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,27 @@
 
 const elementPlace = document.querySelector('.places__list');
-const element = document.querySelector('#card-template').content;
+const element = document.querySelector('#card-template');
+const deleteElement = function (evt) { 
+  evt.target.closest('.card').remove() 
+}
+
 
 initialCards.forEach(function (elements) {
     return renderCard(elements.link, elements.name);
   });
 
-function createElement(imageValue, textValue, deleteElement) {
-    const userElemnt = document.querySelector('#card-template').content;
-    const userElemntPlace = userElemnt.querySelector('.places__item').cloneNode(true);
-    userElemntPlace.querySelector('.card__image').src = imageValue
-    userElemntPlace.querySelector('.card__image').alt = textValue
-    userElemntPlace.querySelector('.card__title').textContent = textValue
+function createElement(imageValue, textValue, deleteElement) { 
+    const userElemntPlace = element.content.querySelector('.places__item').cloneNode(true);
+    const cardImage = userElemntPlace.querySelector('.card__image')
+    const cardTitle =  userElemntPlace.querySelector('.card__title')
+    cardImage.src = imageValue
+    cardImage.alt = textValue
+    cardTitle.textContent = textValue
     const deleteButton = userElemntPlace.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', deleteElement);
     return userElemntPlace;
   }
 
   function renderCard(img, text) {
-    return elementPlace.prepend(createElement(img, text, function (evt) {
-      evt.target.closest('.card').remove() 
-      }));
+    return elementPlace.prepend(createElement(img, text,deleteElement))
   }
